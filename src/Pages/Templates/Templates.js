9557051ee8e2f0cd85ad769609a6767c03b21874
Samuel Trahan <samtrahan91@gmail.com>
2020-11-17 10:48:38 -0600

    three templates added to the template page

--- src/Pages/Templates/Templates.js
+++ src/Pages/Templates/Templates.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Templates.css';
+import Pic06 from '../../Assets/FINESSE.png';
 
 export default function Templates() {
   return (
-    <div>
-      Templates
+    <div className="template-page">
+      <h1 className="heading">Check out the templates I've made using React</h1>
+      <div className="section">
+        <a className="finesse" target="_blank" rel="noreferrer" href="https://finesse.samueltrahan.com/">
+          <section>
+            <h1 className="finesse-head">Finesse</h1>
+          </section>
+        </a>
+        <a className="lion" target="_blank" rel="noreferrer" href="https://lion.samueltrahan.com">
+          <section>
+            <h1 className="lion-head">Lion</h1>
+          </section>
+        </a>
+        <a className="morning" target="_blank" rel="noreferrer" href="https://morning.samueltrahan.com">
+          <section>
+            <h1 className="morning-head">Morning</h1>
+          </section>
+        </a>
+      </div>
     </div>
   )
 }
