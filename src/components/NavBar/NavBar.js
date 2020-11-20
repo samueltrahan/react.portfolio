a4b2c16aba93842283f6b724be6f6b22253814f4
Samuel Trahan <samtrahan91@gmail.com>
2020-11-20 11:36:12 -0600

    some styling fixes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './NavBar.css';
 
-export default function NavBar() {
+export default function NavBar({path}) {
   return (
     <nav className="nav-bar">
       <div className="image-section">
         <a href="/"><img className="image" alt="" src="/images/st.jpg"></img></a>
       </div>
       <div className="nav-links">
-      <a className="links" href="#projects">Projects</a>
+      <a className="links" href={path}>Projects</a>
       <Link className="links" to="/templates">Templates</Link>
       <Link className="links" to="/about">About</Link>
       <a className="links" href="#contact">Contact</a>
